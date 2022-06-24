d1dba57d9a05b4565159a8663ff6a628f5705045
Hita Juneja <hitajuneja@gmail.com>
2022-06-24 13:18:04 +0530

    client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import Test from './components/Test';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Test/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
